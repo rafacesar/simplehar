5384a34b9863c9cefc7c6ff9777a80d2c808d0f2
Rafael Cesar <rafa.cesar@gmail.com>
2014-04-25 00:25:44 -0300

    Better visualization based on status code

--- src/harParser.js
+++ src/harParser.js
@@ -233,7 +233,8 @@ module.exports = function(har, htmlEncode) {
 			windowloaded:onLoadText,
 			_totalTime:totalTime >= 0? totalTime : 0,
 			totalTime:formatSize(totalTime >= 0? totalTime : 0, 2) + 'ms',
-			rId:Math.floor((Math.random()*(new Date()).getTime())+1)
+			rId:Math.floor((Math.random()*(new Date()).getTime())+1),
+			bgstatus: status >= 300?'redirect':status >= 400?'warning':status >= 500?'danger':''
 		};
 		
 	},
