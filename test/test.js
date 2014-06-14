fd87cc27fde967076bc71fff45ff85ec0fee6b3d
Rafael Cesar <rafa.cesar@gmail.com>
2014-06-14 01:03:00 -0300

    Changed the unit tests to use expect.js Finished unit tests of binary part

--- test/test.js
+++ test/test.js
@@ -1,20 +1,54 @@
-var assert = require('assert'),
+var expect = require('expect.js'),
 	execBin = require('../bin/execSimplehar.js');
 
 
-describe('Binary Options', function() {
-	it('Version', function() {
-		assert.equal('0.14.0', execBin(['', '-v']));
+describe('Binary Parameter', function() {
+	it('should show the version', function() {
+		expect(execBin(['', '-v'])).to.be('0.14.0');
 	});
-	it('Help', function() {
-		assert.equal('--help', execBin(['', '--help']));
+	it('should show the help message', function() {
+		expect(execBin(['', '--help'])).to.be('--help');
 	});
-	it('Simple Call', function() {
-		assert
-			.deepEqual({
-				har:'test.har',
-				html:'test.html',
-				lng:process.env.LANG || false
-			}, execBin(['', 'test.har']));
+	it('should throw an error if without or with empty parameters', function() {
+		expect(execBin).withArgs().to.throwException(function(error) {
+			expect(error).to.be.a(TypeError);
+		});
+		expect(execBin).withArgs('').to.throwException(function(error) {
+			expect(error).to.be.a(TypeError);
+		});
+	});
+	it('show return the simplest object', function() {
+		expect(execBin(['', 'test.har'])).to.eql({
+			har:'test.har',
+			html:'test.html',
+			lng:process.env.LANG || false
+		});
+	});
+	it('should return an object with different html path', function() {
+		expect(execBin(['', 'test.har', 'testing.html'])).to.eql({
+			har:'test.har',
+			html:'testing.html',
+			lng:process.env.LANG || false
+		});
+		expect(execBin(['', 'test.har', 'testing.htm'])).to.eql({
+			har:'test.har',
+			html:'testing.htm',
+			lng:process.env.LANG || false
+		});
+	});
+	it('should return a specific language', function() {
+		expect(execBin(['', 'test.har', 'lng=en-US'])).to.eql({
+			har:'test.har',
+			html:'test.html',
+			lng:'en-US'
+		});
+	});
+	it('should return with a frame attribute', function() {
+		expect(execBin(['', 'test.har', 'frame'])).to.eql({
+			har:'test.har',
+			html:'test.html',
+			lng:process.env.LANG || false,
+			frame:true
+		});
 	});
 });
